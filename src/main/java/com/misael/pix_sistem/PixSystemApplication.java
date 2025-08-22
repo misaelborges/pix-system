5435e1d2050f49dd750230cb9b3395a81f0319ea
Misael Borges Cancelier <mizaelborges2011@gmail.com>
2025-08-22 13:19:57 -0300

    projeto criado

---
+++ src/main/java/com/misael/pix_sistem/PixSystemApplication.java
@@ -0,0 +1,13 @@
+package com.misael.pix_sistem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PixSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PixSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/misael/pix_sistem/PixSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.misael.pix_sistem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PixSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
