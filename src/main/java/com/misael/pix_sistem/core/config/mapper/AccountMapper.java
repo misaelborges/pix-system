486c1015a6b60f140f66c5164ca551eaefdc83f3
Misael Borges Cancelier <mizaelborges2011@gmail.com>
2025-08-24 12:33:47 -0300

    feature: cria o metodo converter entidade em AccountBalanceResponseDTO

--- src/main/java/com/misael/pix_sistem/core/config/mapper/AccountMapper.java
+++ src/main/java/com/misael/pix_sistem/core/config/mapper/AccountMapper.java
@@ -1,6 +1,7 @@
 package com.misael.pix_sistem.core.config.mapper;
 
 import com.misael.pix_sistem.api.dto.request.AccountRequestDTO;
+import com.misael.pix_sistem.api.dto.request.AccountUpdateRequestDTO;
 import com.misael.pix_sistem.api.dto.response.AccountBalanceResponseDTO;
 import com.misael.pix_sistem.api.dto.response.AccountsResponseDTO;
 import com.misael.pix_sistem.api.dto.response.AccountsResumeResponseDTO;
@@ -16,4 +17,5 @@ public interface AccountMapper {
     AccountsResponseDTO toDto(Accounts accounts);
     List<AccountsResumeResponseDTO> toListResponse(List<Accounts> accounts);
     AccountBalanceResponseDTO balanceToDto(Accounts accounts);
+    Accounts toEntity(AccountUpdateRequestDTO accountRequestDTO);
 }
