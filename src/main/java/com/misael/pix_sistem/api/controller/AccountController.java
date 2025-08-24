bddea46b4447e4da6c46946dee4ecf0de0c69926
Misael Borges Cancelier <mizaelborges2011@gmail.com>
2025-08-24 10:28:48 -0300

    feature: cria Account Rest controller

---
+++ src/main/java/com/misael/pix_sistem/api/controller/AccountController.java
@@ -0,0 +1,42 @@
+package com.misael.pix_sistem.api.controller;
+
+import com.misael.pix_sistem.api.assemblers.AccountResponseAssembler;
+import com.misael.pix_sistem.api.dto.request.AccountRequestDTO;
+import com.misael.pix_sistem.api.dto.response.AccountsResponseDTO;
+import com.misael.pix_sistem.core.config.mapper.AccountMapper;
+import com.misael.pix_sistem.domain.model.Accounts;
+import com.misael.pix_sistem.domain.service.impl.AccountServiceImpl;
+import jakarta.validation.Valid;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/accounts")
+public class AccountController {
+
+    private final AccountServiceImpl accountService;
+    private final AccountMapper accountMapper;
+    private final AccountResponseAssembler responseAssembler;
+
+    public AccountController(AccountServiceImpl accountService, AccountMapper mapStructConfig, AccountResponseAssembler responseAssembler) {
+        this.accountService = accountService;
+        this.accountMapper = mapStructConfig;
+        this.responseAssembler = responseAssembler;
+    }
+
+    @PostMapping
+    public ResponseEntity<EntityModel<AccountsResponseDTO>> createAccount(@RequestBody @Valid AccountRequestDTO accountRequestDTO) {
+        Accounts accounts = accountMapper.toEntity(accountRequestDTO);
+        accounts = accountService.createAccount(accounts);
+        return ResponseEntity.status(HttpStatus.CREATED).body(responseAssembler.toModel(accounts));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<EntityModel<AccountsResponseDTO>> findAccountById(@PathVariable Long id) {
+        Accounts accounts = accountService.findAccountById(id);
+        accounts = accountService.createAccount(accounts);
+        return ResponseEntity.ok(responseAssembler.toModel(accounts));
+    }
+}
