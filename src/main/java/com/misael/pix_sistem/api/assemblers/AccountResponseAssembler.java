84bd6b470a0663172f6d2adc246e3d355203dc95
Misael Borges Cancelier <mizaelborges2011@gmail.com>
2025-08-24 10:27:18 -0300

    feature: implementa hateos para response de Account

---
+++ src/main/java/com/misael/pix_sistem/api/assemblers/AccountResponseAssembler.java
@@ -0,0 +1,29 @@
+package com.misael.pix_sistem.api.assemblers;
+
+import com.misael.pix_sistem.api.controller.AccountController;
+import com.misael.pix_sistem.api.dto.response.AccountsResponseDTO;
+import com.misael.pix_sistem.core.config.mapper.AccountMapper;
+import com.misael.pix_sistem.domain.model.Accounts;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.stereotype.Component;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
+@Component
+public class AccountResponseAssembler {
+
+    private final AccountMapper accountMapper;
+
+    public AccountResponseAssembler(AccountMapper accountMapper) {
+        this.accountMapper = accountMapper;
+    }
+
+    public EntityModel<AccountsResponseDTO> toModel(Accounts accounts) {
+        AccountsResponseDTO accountsResponseDTO = accountMapper.toDto(accounts);
+
+        return EntityModel.of(accountsResponseDTO)
+                .add(linkTo(methodOn(AccountController.class).findAccountById(accounts.getId())).withSelfRel())
+                .add(linkTo(AccountController.class).withRel("accounts"));
+    }
+}
