60d0396b9f774052b0d240599445aa9cdd81a0c0
Misael Borges <mizaelborges2011@gmail.com>
2026-01-18 15:23:38 -0300

    refatora tabela transactionas_table

--- src/main/resources/db/migration/V3__create_transactions_table.sql
+++ src/main/resources/db/migration/V3__create_transactions_table.sql
@@ -1,13 +1,11 @@
 CREATE TABLE transactions (
     id BIGSERIAL PRIMARY KEY,
-    from_account_id BIGINT NOT NULL,
-    to_account_id BIGINT NOT NULL,
-    pix_key_used VARCHAR(255),
+    sender_id BIGINT NOT NULL,
+    receiver_id BIGINT NOT NULL,
+    pix_key_receiver VARCHAR(255),
     amount DECIMAL(15,2) NOT NULL,
     description TEXT,
-    status VARCHAR(20) DEFAULT 'COMPLETED',
-    transaction_type VARCHAR(20) DEFAULT 'PIX_TRANSFER',
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (from_account_id) REFERENCES accounts(id),
-    FOREIGN KEY (to_account_id) REFERENCES accounts(id)
+    FOREIGN KEY (sender_id) REFERENCES accounts(id),
+    FOREIGN KEY (receiver_id) REFERENCES accounts(id)
 );
